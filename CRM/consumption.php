ee2d11e676280cc6928769ef1369f40c59bfbeec
gcobs120399 <gcobs120399@gmail.com>
2018-05-30 18:01:42 +0800

    網頁架構+移動檔案

---
+++ CRM/consumption.php
@@ -0,0 +1,111 @@
+<?php
+header("Content-Type: text/html; charset=utf-8");
+require_once("MYSQL.php");
+session_start();
+//檢查是否經過登入
+if(!isset($_SESSION["loginMember"]) || ($_SESSION["loginMember"]=="")){
+  header("Location: index.php");
+}
+//執行登出動作
+if(isset($_GET["logout"]) && ($_GET["logout"]=="true")){
+  unset($_SESSION["loginMember"]);
+  unset($_SESSION["memberLevel"]);
+  header("Location: index.php");
+}
+//繫結登入會員資料
+$query_RecMember = "SELECT * FROM `memberdata` WHERE `m_username`='".$_SESSION["loginMember"]."'";
+$RecMember = mysql_query($query_RecMember);
+$row_RecMember=mysql_fetch_assoc($RecMember);
+?>
+<html lang="en">
+<head>
+  <title>顧客關係管理之寵物飼料管理</title>
+<!--呆的巡覽列-->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="./css/bootstrap.min.css" rel="stylesheet">
+  <script src="./js/ie-emulation-modes-warning.js"></script> 
+  <link rel="icon" href="./img/title.png">
+<!--呆-->
+  <meta  http-equiv="Content-Type" content="text/html;charset=utf-8">
+  <link href="style.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" type="text/css" href="css/menu.css"><!--菜單CSS+頂端-->
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+</head>
+<body style="text-align:left;font-size:18px;background-image: url(img/46505.png);background-size: cover;background-attachment: fixed; font-family: 微軟正黑體;margin:30px">
+<!--巡覽列black-->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header"> 
+     <a class="navbar-brand" href="member_center.php" style="font-size: 24pt;">顧客關係管理之寵物飼料管理</a>
+    </div>
+  </div>
+</nav>
+<!--巡覽列white-->
+<nav class="navbar navbar-default navbar-fixed-top" style="top: 50px;" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> 
+          <span class="sr-only"></span> 
+          <span class="icon-bar"></span> 
+          <span class="icon-bar"></span> 
+          <span class="icon-bar"></span> 
+        </button>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav" style="font-size: 20px;">
+        <li><a href="member.php">客群分析</a></li>
+        <li><a href="user.php">使用者行為</a></li>
+        <li class="active"><a href="consumption.php">消費行為</a></li>
+        <li><a href="personal.php">個人化行銷</a></li>
+        <li><a href="sell.php">行銷分析</a></li>
+        <li><a href="?logout=true">登出</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class=" col-xs-1 col-md-1"></div>
+<div class="container col-xs-10 col-md-10">
+  <!--內文-->
+  <h1 style="text-align:center;">顧客關係管理之寵物飼料管理</h1>
+  <div style="background: rgba(100%,100%,100%,0.6); margin: 0 auto;"><!--div放白色背景透明度60%開始-->
+    <div style="text-align: center"><h1>消費行為</h1></div>
+    <div style="margin-left:0px auto;margin-right:0px auto;">
+      <div style="display: table-cell;vertical-align: middle;">
+        <table>
+          <tr>
+          <td style="font-size: 30px">
+           
+            </p>
+          </td>
+          </tr>
+        </table>
+    </div>
+    <div style="display: table-cell;vertical-align: middle;"></div>
+</div>
+
+  <footer align="center" style="font-size: 18px;">© 2018 顧客關係管理之寵物飼料管理 ©</footer>
+</div><!--div放白色透明度60%結束-->
+</div>
+<!--呆的巡覽列-->
+<script src="./js/jquery.min.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+<script src="./js/ie10-viewport-bug-workaround.js"></script>
+<!--呆-->
+</body>
+<script type="text/javascript">/*這為左邊菜單的JS，來源http://codepen.io/vkbansal/pen/QbapGz*/
+  'use strict';
+var burger = document.getElementById('burger-button');
+burger.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.body.classList.toggle('open');
+    burger.classList.toggle('open');
+});
+</script>
+</html>
---
+++ CRM/memberdata.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- 主機: 127.0.0.1
+-- 產生時間： 2018-05-30 10:23:51
+-- 伺服器版本: 10.1.13-MariaDB
+-- PHP 版本： 5.6.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `phpmember`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `memberdata`
+--
+
+CREATE TABLE `memberdata` (
+  `m_id` int(255) NOT NULL,
+  `m_name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `m_username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `m_passwd` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `m_sex` enum('男','女') COLLATE utf8_unicode_ci NOT NULL,
+  `m_birthday` date DEFAULT NULL,
+  `m_level` enum('admin','member') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'member',
+  `m_email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `m_url` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `m_phone` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `m_address` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `m_login` int(255) UNSIGNED NOT NULL DEFAULT '0',
+  `m_logintime` datetime DEFAULT NULL,
+  `m_jointime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 資料表的匯出資料 `memberdata`
+--
+
+INSERT INTO `memberdata` (`m_id`, `m_name`, `m_username`, `m_passwd`, `m_sex`, `m_birthday`, `m_level`, `m_email`, `m_url`, `m_phone`, `m_address`, `m_login`, `m_logintime`, `m_jointime`) VALUES
+(1, '系統管理員', 'admin', '21232f297a57a5a743894a0e4a801fc3', '男', NULL, 'admin', NULL, NULL, NULL, NULL, 7, '2008-10-21 12:07:13', '2008-10-20 16:36:15'),
+(12, '9527', 'gcobs120399', '4f58111f49e05226933bf1122df4a9ae', '女', '1996-01-20', 'member', 'gcobs120399@gmail.com', '', '', '', 90, '2017-04-27 14:32:49', '2016-07-04 13:18:25'),
+(13, '113', 'root12', 'f5948d35adf81357375ec2fe1313259f', '女', '1235-12-23', 'admin', 'gcobs120399@gmail.com', '', '', '', 18, '2016-10-22 09:04:15', '2016-07-04 13:29:24'),
+(14, '豐田蘭園', 'b10356019', 'aef24e1def02692cefab5a014a8770d5', '女', '2017-01-03', 'member', '123@yahoo.com.tw', '', '', '', 115, '2017-12-13 07:50:08', '2016-11-14 18:17:10'),
+(15, '萬俟和玉', 'adg257p', '377c92bbe8b81606ed934808d2afa2cc', '女', '1996-07-11', 'member', 'adg257p@yahoo.com.tw', '', '', '', 1, '2016-11-15 16:41:11', '2016-11-15 16:41:03'),
+(16, 'HTML5', 'abcde', '827ccb0eea8a706c4c34a16891f84e7b', '女', '0000-00-00', 'member', 'emppy_miao511311@yahoo.com.tw', '', '', '', 35, '2017-04-11 16:24:59', '2016-11-15 16:46:20'),
+(17, 'rybfvevk', 'uqubbetw', '32cc5886dc1fa8c106a02056292c4654', '女', '0000-00-00', 'member', 'sample@email.tst', '1', '555-666-0606', '3137 Laguna Street', 0, NULL, '2017-06-02 13:55:27'),
+(18, 'ruskxfcj', 'jxuitvgr', '32cc5886dc1fa8c106a02056292c4654', '男', '0000-00-00', 'member', 'sample@email.tst', '1', '555-666-0606', '3137 Laguna Street', 0, NULL, '2017-06-02 13:55:27');
+
+--
+-- 已匯出資料表的索引
+--
+
+--
+-- 資料表索引 `memberdata`
+--
+ALTER TABLE `memberdata`
+  ADD PRIMARY KEY (`m_id`),
+  ADD UNIQUE KEY `m_username` (`m_username`);
+
+--
+-- 在匯出的資料表使用 AUTO_INCREMENT
+--
+
+--
+-- 使用資料表 AUTO_INCREMENT `memberdata`
+--
+ALTER TABLE `memberdata`
+  MODIFY `m_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
