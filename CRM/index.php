5726adac61fcaefcc1f199bc29c571faa457fad9
gcobs120399 <gcobs120399@gmail.com>
2018-05-31 11:02:06 +0800

    更新

--- CRM/index.php
+++ CRM/index.php
@@ -5,7 +5,7 @@ session_start();
 //檢查是否經過登入，若有登入則重新導向
 if(isset($_SESSION["loginMember"]) && ($_SESSION["loginMember"]!="")){
   /*if($_SESSION["memberLevel"]=="member"){*/
-    header("Location: member.php");
+    header("Location: user.php");
   /*}else{
     header("Location: member_admin.php");
   }*/
