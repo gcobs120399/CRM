70bbde1296a59b56a77d72403fdfd4f3d00f153e
gcobs120399 <gcobs120399@gmail.com>
2018-06-01 12:54:32 +0800

    更新

--- CRM/member_path.php
+++ CRM/member_path.php
@@ -16,6 +16,34 @@ if(isset($_GET["logout"]) && ($_GET["logout"]=="true")){
 $query_RecMember = "SELECT * FROM `memberdata` WHERE `m_username`='".$_SESSION["loginMember"]."'";
 $RecMember = mysql_query($query_RecMember);
 $row_RecMember=mysql_fetch_assoc($RecMember);
+
+$query_RecFlower = "SELECT * FROM `path`";
+$RecFlower = mysql_query($query_RecFlower);
+$row_RecFlower=mysql_fetch_assoc($RecFlower);
+//選取所有一般會員資料
+//預設每頁筆數
+$pageRow_records = 12;
+//預設頁數
+$num_pages = 1;
+//若已經有翻頁，將頁數更新
+if (isset($_GET['page'])) {
+  $num_pages = $_GET['page'];
+}
+//本頁開始記錄筆數 = (頁數-1)*每頁記錄筆數
+$startRow_records = ($num_pages -1) * $pageRow_records;
+//未加限制顯示筆數的SQL敘述句
+$query_RecFlower = "SELECT * FROM `path`";
+//加上限制顯示筆數的SQL敘述句，由本頁開始記錄筆數開始，每頁顯示預設筆數
+$query_limit_RecFlower = $query_RecFlower." LIMIT ".$startRow_records.", ".$pageRow_records;
+//以加上限制顯示筆數的SQL敘述句查詢資料到 $resultMember 中
+$RecFlower = mysql_query($query_limit_RecFlower);
+//以未加上限制顯示筆數的SQL敘述句查詢資料到 $all_resultMember 中
+$all_RecFlower = mysql_query($query_RecFlower);
+//計算總筆數
+$total_records = mysql_num_rows($all_RecFlower);
+//計算總頁數=(總筆數/每頁筆數)後無條件進位。
+$total_pages = ceil($total_records/$pageRow_records);
+
 ?>
 <html lang="en">
 <head>
