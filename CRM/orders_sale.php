74a5e2f6083f031ffd38192eac6be5d3b888e998
gcobs120399 <gcobs120399@gmail.com>
2018-06-04 13:32:37 +0800

    資料庫更新

--- CRM/orders_sale.php
+++ CRM/orders_sale.php
@@ -17,7 +17,7 @@ $query_RecMember = "SELECT * FROM `memberdata` WHERE `m_username`='".$_SESSION["
 $RecMember = mysql_query($query_RecMember);
 $row_RecMember=mysql_fetch_assoc($RecMember);
 
-$query_RecFlower = "SELECT * FROM `order1`";
+$query_RecFlower = "SELECT * FROM `food` ORDER BY `o_no`,`buff`";
 $RecFlower = mysql_query($query_RecFlower);
 $row_RecFlower=mysql_fetch_assoc($RecFlower);
 //選取所有一般會員資料
@@ -32,7 +32,7 @@ if (isset($_GET['page'])) {
 //本頁開始記錄筆數 = (頁數-1)*每頁記錄筆數
 $startRow_records = ($num_pages -1) * $pageRow_records;
 //未加限制顯示筆數的SQL敘述句
-$query_RecFlower = "SELECT * FROM `order1` ORDER BY `o_no`,`date` ";
+$query_RecFlower = "SELECT * FROM food ";
 //加上限制顯示筆數的SQL敘述句，由本頁開始記錄筆數開始，每頁顯示預設筆數
 $query_limit_RecFlower = $query_RecFlower." LIMIT ".$startRow_records.", ".$pageRow_records;
 //以加上限制顯示筆數的SQL敘述句查詢資料到 $resultMember 中
@@ -43,15 +43,6 @@ $all_RecFlower = mysql_query($query_RecFlower);
 $total_records = mysql_num_rows($all_RecFlower);
 //計算總頁數=(總筆數/每頁筆數)後無條件進位。
 $total_pages = ceil($total_records/$pageRow_records);
-
-
-
-
-for ($i=0; $i <12 ; $i++) {
-  $pre[$i]=rand(100,150);}
-for ($i=0; $i <12 ; $i++) {
-  $pre1[$i]=rand(100,150);}
-$pre = json_encode($pre);
 
 ?>
 <html lang="en">
@@ -108,7 +99,7 @@ $pre = json_encode($pre);
   </div>
 </nav>
 <br><br><br>
-<h1 style="text-align:center;">訂單金額分析</h1>
+<h1 style="text-align:center;">促銷成效分析</h1>
 <hr>
 <div class=" col-xs-3 col-md-3" style="background: rgba(100%,100%,100%,0.6); margin: 0 auto;">
   <a href="orders.php" style="text-align:center;font-size: 30px;font-family: 微軟正黑體;font-weight: bold;color: red"><img src="newimg/20.png" alt="LOGO" width="80" height="50">訂單金額分析</a><br>
@@ -127,13 +118,12 @@ $pre = json_encode($pre);
     <td class="tdbline">
     <table width="100%" border="0px" cellspacing="0" cellpadding="10" style="font-size: 20px;">
       <tr valign="top">
-        <td class="tdrline"><p class="title" style="text-align: center;font-size: 24px;">訂單金額分析</p>
+        <td class="tdrline"><p class="title" style="text-align: center;font-size: 24px;">促銷成效分析</p>
           <table width="100%"  border="1px" cellpadding="0" cellspacing="0" bgcolor="#F0F0F0" >
             <tr >
-              <th width="10%" bgcolor="#81D4FA" style="text-align:center;font-size: 20px;"><p>顧客編號</p></th>
-              <th width="10%" bgcolor="#81D4FA" style="text-align:center;font-size: 20px;"><p>性別</p></th>
-              <th width="10%" bgcolor="#81D4FA" style="text-align:center;font-size: 20px;"><p>年齡</p></th>
-              <th width="10%" bgcolor="#81D4FA" style="text-align:center;font-size: 20px;"><p>金額</p></th>
+              <th width="10%" bgcolor="#81D4FA" style="text-align:center;font-size: 20px;"><p>編號</p></th>
+              <th width="10%" bgcolor="#81D4FA" style="text-align:center;font-size: 20px;"><p>日期</p></th>
+              <th width="10%" bgcolor="#81D4FA" style="text-align:center;font-size: 20px;"><p>是否有促銷</p></th>
             </tr>
       <?php while($row_RecFlower=mysql_fetch_assoc($RecFlower)){ ?>
             <tr>
@@ -142,20 +132,17 @@ $pre = json_encode($pre);
               </td>
               <td width="10%" align="center" bgcolor="#FFFFFF" style="font-size: 20px;"><p>
                 <?php echo $row_RecFlower["sex"]; ?>
-                </p></td>
-                <td width="10%" align="center" bgcolor="#FFFFFF" style="font-size: 20px;"><p>
-                <?php echo $row_RecFlower["ago"]; ?>
-                </p></td>
-                <td width="10%" align="center" bgcolor="#FFFFFF" style="font-size: 20px;"><p>
-                <?php echo $row_RecFlower["money"]; ?>
-                </p></td>
+              </p></td>
+              <td width="10%" align="center" bgcolor="#FFFFFF" style="font-size: 20px;"><p>
+                <?php echo $row_RecFlower["buff"]; ?>
+              </p></td>
+            <?php }?>
             </tr>
-      <?php }?>
       <hr size="1" />
           <table width="98%" border="0px" align="center" cellpadding="4" cellspacing="0">
             <tr>
-              <td valign="middle"><p style="font-size: 20px;">資料筆數：<?php echo $total_records;?></p></td>
-              <td align="right"><p style="font-size: 20px;">
+              <td valign="middle"><p>資料筆數：<?php echo $total_records;?></p></td>
+              <td align="right"><p>
                   <?php if ($num_pages > 1) { // 若不是第一頁則顯示 ?>
                   <a href="?page=1">第一頁</a> | <a href="?page=<?php echo $num_pages-1;?>">上一頁</a> |
                 <?php }?>
@@ -165,7 +152,6 @@ $pre = json_encode($pre);
               </p></td>
             </tr>
           </table>
-
     <div style="display: table-cell;vertical-align: middle;"></div>
 </div>
 </div><!--div放白色透明度60%結束-->
@@ -188,14 +174,26 @@ burger.addEventListener('click', function (e) {
 });
 </script>
 <script>
-var chart = Highcharts.chart('container', {
+var chart = Highcharts.chart('container',{
+    chart: {
+        type: 'column'
+    },
     title: {
-        text: '訂單金額分析',
+        text: '合買分析',
         style:{
             fontSize:'24px'
         }
     },
     subtitle: {
+    },
+    xAxis: {
+        categories: ['增重','腎臟管理','血糖管理','肥胖代謝'],
+        crosshair: true,
+        labels:{
+            style:{
+                fontSize:'18px'
+            }
+        }
     },
     yAxis: {
         title: {
@@ -205,67 +203,40 @@ var chart = Highcharts.chart('container', {
             }
         },
         labels:{
-              style:{
-                fontSize:'18px'
-              }
-            }
-    },xAxis: {
-        categories: ['400-450', '451-500', '501-550', '551-600'],
-        title: {
-            text: '價格',
             style:{
                 fontSize:'18px'
             }
-        },
-        labels:{
-              style:{
-                fontSize:'18px'
-              }
-            }
+        }
     },
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
+    tooltip: {
+        // head + 每个 point + footer 拼接成完整的 table
+        headerFormat: '<span style="font-size:14px">{point.key}</span><table><br>',
+        pointFormat: '{series.name}:' +'{point.y:.1f} 人<br>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
     },
     plotOptions: {
-        bar: {
-            dataLabels: {
-                enabled: true,
-                allowOverlap: true // 允许数据标签重叠
-            }
+        column: {
+            borderWidth: 0
         }
     },
     series: [{
-        name: '20以下',
-        data: [1, 0, 2, 2]
+        name: '優寶心',
+        data: [5, 7, 1, 1]
     }, {
-        name: '21-30',
-        data: [9, 4, 5, 5]
+        name: '優寶睛',
+        data: [8, 5, 9, 3]
     }, {
-        name: '31-40',
-        data: [6, 4, 3, 7]
+        name: '優寶身',
+        data: [ 4, 4, 4, 10]
     }, {
-        name: '41-50',
-        data: [4, 3, 2, 4]
+        name: '優寶骨',
+        data: [ 7, 5, 4, 4]
     }, {
-        name: '51以上',
-        data: [18,10,8, 5]
-    }],
-    responsive: {
-        rules: [{
-            condition: {
-                maxWidth: 500
-            },
-            chartOptions: {
-                legend: {
-                    layout: 'horizontal',
-                    align: 'center',
-                    verticalAlign: 'bottom',
-                }
-            }
-        }],
-    }
+        name: '優寶膚',
+        data: [ 6, 6, 1, 6]
+    }]
 });
 </script>
 </html>
